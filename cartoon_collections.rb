1133e420cf3667f0e230474cba348689c753685a
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-23 21:20:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)
-  array.each do |name| print name
-  end
+array.each_with_index { |item, index|
+  hash[item] = index
+}
 end
 
 def summon_captain_planet# code an argument here
