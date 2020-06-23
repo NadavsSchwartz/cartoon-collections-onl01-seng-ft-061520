378c1c4883ce91021d711a80ae2273eac82002e1
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-23 21:46:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,11 @@ def summon_captain_planet(arr)
 end
 
 def long_planeteer_calls(array)
-  array.detect {|i| i.length > 4}
+  if array.detect {|i| i.length > 4}
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
